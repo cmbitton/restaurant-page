900ca6bb5a4eb7bdf5e30bb4ba7bbc4a56e5dcf6
Curtis <cmbitton@gmail.com>
2023-01-24 15:23:59 -0500

    Finish contact page and touch up CSS.
    Project is nearly finished, all pages are filled out and just need minor
    tweaks. Mobile layout is the biggest problem, along with form validation
    and some other areas that need work.

--- src/contact.js
+++ src/contact.js
@@ -16,6 +16,7 @@ export default class Contact {
         this.bookHeading.textContent = 'RESERVATIONS';
         this.content.append(this.contactHeading, this.contactContainer);
         this.showBookTableForm();
+        this.showOpeningHours();
     }
 
     showBookTableForm () {
@@ -28,6 +29,8 @@ export default class Contact {
         firstNameInput.type = 'text';
         firstNameInput.name = 'firstname';
         firstNameInput.id = 'firstname';
+        firstNameInput.setAttribute('autocomplete', 'name');
+        firstNameInput.setAttribute('placeholder', 'First Name');
         firstNameLabel.setAttribute('for', 'firstname');
         firstNameLabel.textContent = 'First Name';
 
@@ -42,6 +45,8 @@ export default class Contact {
         lastNameInput.type = 'text';
         lastNameInput.name = 'lastname';
         lastNameInput.id = 'lastname';
+        lastNameInput.setAttribute('autocomplete', 'name');
+        lastNameInput.setAttribute('placeholder', 'Last Name');
         lastNameLabel.setAttribute('for', 'lastname');
         lastNameLabel.textContent = 'Last Name';
 
@@ -56,14 +61,120 @@ export default class Contact {
         phoneInput.type = 'tel';
         phoneInput.name = 'phone-number';
         phoneInput.id = 'phone-number';
+        phoneInput.setAttribute('placeholder', '123-456-7890');
         phoneLabel.setAttribute('for', 'phone-number');
         phoneLabel.textContent = 'Phone Number';
 
         phoneContainer.append(phoneLabel, phoneInput);
 
+        //email input
+        const emailContainer = document.createElement('div');
+        const emailInput = document.createElement('input');
+        const emailLabel = document.createElement('label');
 
-        this.bookTableForm.append(firstNameContainer, lastNameContainer);
+        emailContainer.classList.add('email-container');
+        emailInput.type = 'email';
+        emailInput.name = 'email';
+        emailInput.id = 'email';
+        emailInput.setAttribute('placeholder', 'username@email.com');
+        emailLabel.setAttribute('for', 'email');
+        emailLabel.textContent = 'Email';
+
+        emailContainer.append(emailLabel, emailInput);
+
+        //date input
+        const dateContainer = document.createElement('div');
+        const dateInput = document.createElement('input');
+        const dateLabel = document.createElement('label');
+
+        dateContainer.classList.add('date-container');
+        dateInput.type = 'date';
+        dateInput.name = 'date';
+        dateInput.id = 'date';
+        dateLabel.setAttribute('for', 'date');
+        dateLabel.textContent = 'Date';
+
+        dateContainer.append(dateLabel, dateInput);
+
+        //time input
+        const timeContainer = document.createElement('div');
+        const timeInput = document.createElement('input');
+        const timeLabel = document.createElement('label');
+
+        timeContainer.classList.add('time-container');
+        timeInput.type = 'time';
+        timeInput.name = 'time';
+        timeInput.id = 'time';
+        timeLabel.setAttribute('for', 'time');
+        timeLabel.textContent = 'Time';
+
+        timeContainer.append(timeLabel, timeInput);
+
+
+        this.bookTableForm.append(firstNameContainer, lastNameContainer, phoneContainer, emailContainer, dateContainer, timeContainer);
         this.bookTableContainer.append(this.bookHeading, this.bookTableForm);
         this.contactContainer.append(this.bookTableContainer);
+
+        //submit button
+        const reservationSubmitButton = document.createElement('button');
+        reservationSubmitButton.classList.add('reservation-submit-button');
+        reservationSubmitButton.setAttribute('type', 'submit');
+        reservationSubmitButton.textContent = 'Submit Reservation'
+        this.bookTableForm.append(reservationSubmitButton);
+    }
+
+    showOpeningHours() {
+        const hoursContainer = document.createElement('div');
+        const hoursHeading = document.createElement('h2');
+        const hoursText = document.createElement('p');
+        hoursContainer.classList.add('hours-container');
+        hoursHeading.classList.add('hours-heading');
+        hoursText.classList.add('hours-text');
+
+        hoursHeading.textContent = 'OPENING HOURS';
+        hoursText.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, inventore hic nulla, provident laudantium voluptate ipsa eligendi commodi mollitia vero animi, repudiandae quidem minima est sint sapiente quas iure deleniti.'
+
+        //weekday hours
+        const weekdayHoursContainer = document.createElement('div');
+        const weekdayLabel = document.createElement('h3');
+        const weekdayHours = document.createElement('p');
+
+        weekdayHoursContainer.classList.add('weekday-hours-container');
+        weekdayHours.classList.add('weekday-hours');
+        
+        weekdayLabel.textContent = 'Monday - Friday';
+        weekdayHours.textContent = '11AM - 9PM';
+
+        weekdayHoursContainer.append(weekdayLabel, weekdayHours);
+
+        //saturday hours
+        const saturdayHoursContainer = document.createElement('div');
+        const saturdayLabel = document.createElement('h3');
+        const saturdayHours = document.createElement('p');
+
+        saturdayHoursContainer.classList.add('saturday-hours-container');
+        saturdayHours.classList.add('saturday-hours');
+        
+        saturdayLabel.textContent = 'Saturday';
+        saturdayHours.textContent = '8AM - 11PM';
+       
+        saturdayHoursContainer.append(saturdayLabel, saturdayHours);
+
+        //sunday hours
+        const sundayHoursContainer = document.createElement('div');
+        const sundayLabel = document.createElement('h3');
+        const sundayHours = document.createElement('p');
+
+        sundayHoursContainer.classList.add('sunday-hours-container');
+        sundayHours.classList.add('sunday-hours');
+        
+        sundayLabel.textContent = 'Sunday';
+        sundayHours.textContent = '8AM - 3PM';
+       
+        sundayHoursContainer.append(sundayLabel, sundayHours);
+
+        hoursContainer.append(hoursHeading, hoursText, weekdayHoursContainer, saturdayHoursContainer, sundayHoursContainer);
+
+        this.contactContainer.append(hoursContainer);
     }
 }
