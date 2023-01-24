1ae19e8ffaf2618c145c1ed7870be6abe95471c1
Curtis <cmbitton@gmail.com>
2023-01-23 22:11:12 -0500

    Begin adding contact page and reservation form

--- src/contact.js
+++ src/contact.js
@@ -19,6 +19,50 @@ export default class Contact {
     }
 
     showBookTableForm () {
+        //first name input
+        const firstNameContainer = document.createElement('div');
+        const firstNameInput = document.createElement('input');
+        const firstNameLabel = document.createElement('label');
+
+        firstNameContainer.classList.add('first-name-container');
+        firstNameInput.type = 'text';
+        firstNameInput.name = 'firstname';
+        firstNameInput.id = 'firstname';
+        firstNameLabel.setAttribute('for', 'firstname');
+        firstNameLabel.textContent = 'First Name';
+
+        firstNameContainer.append(firstNameLabel, firstNameInput)
+
+        //last name input
+        const lastNameContainer = document.createElement('div');
+        const lastNameInput = document.createElement('input');
+        const lastNameLabel = document.createElement('label');
+
+        lastNameContainer.classList.add('last-name-container');
+        lastNameInput.type = 'text';
+        lastNameInput.name = 'lastname';
+        lastNameInput.id = 'lastname';
+        lastNameLabel.setAttribute('for', 'lastname');
+        lastNameLabel.textContent = 'Last Name';
+
+        lastNameContainer.append(lastNameLabel, lastNameInput);
+
+        //phone input
+        const phoneContainer = document.createElement('div');
+        const phoneInput = document.createElement('input');
+        const phoneLabel = document.createElement('label');
+
+        phoneContainer.classList.add('phone-num-container');
+        phoneInput.type = 'tel';
+        phoneInput.name = 'phone-number';
+        phoneInput.id = 'phone-number';
+        phoneLabel.setAttribute('for', 'phone-number');
+        phoneLabel.textContent = 'Phone Number';
+
+        phoneContainer.append(phoneLabel, phoneInput);
+
+
+        this.bookTableForm.append(firstNameContainer, lastNameContainer);
         this.bookTableContainer.append(this.bookHeading, this.bookTableForm);
         this.contactContainer.append(this.bookTableContainer);
     }
